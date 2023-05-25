54b8d82095c4956f0cb3406d77da621ddf7b4faf
Daniel Gleeson <dangleeson13@gmail.com>
2023-05-25 17:40:01 +0100

    Changed visibility
    Made with Flask

--- webserver.py
+++ webserver.py
@@ -1,1 +1,15 @@
-# Removed! :sad: 
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def main():
+    return "Discord Bot Voltage - SlashCMD update, is sussefully online! \n Progress 100% \n latency: n/a"
+
+def run():
+    app.run(host="0.0.0.0", port=0000)
+    
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
